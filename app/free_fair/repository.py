751a484d2e46ab282225aaf27ae828a746f98f8b
Eduardo Pereira <eduardoaugusto93@gmail.com>
2020-12-08 10:32:20 -0300

    Removed Project

--- app/free_fair/repository.py
+++
@@ -1,8 +0,0 @@
-from .config import engine
-from .model import Fair
-
-
-class FreeFairRepository:
-
-    def find_all(self):
-        return engine.execute('SELECT * FROM feira_livre').all()
--- app/free_fair/service.py
+++
@@ -1,7 +0,0 @@
-from .repository import FreeFairRepository
-
-
-class FreeFairService:
-
-    def find_all(self):
-        return FreeFairRepository().find_all()
