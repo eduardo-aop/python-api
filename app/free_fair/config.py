17a0af593e1a8ddc215f9c99b401baa78600630c
Eduardo Pereira <eduardoaugusto93@gmail.com>
2020-12-08 10:33:07 -0300

    Added Project

---
+++ app/free_fair/config.py
@@ -0,0 +1,27 @@
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+
+# user = os.environ['POSTGRES_USER']
+# password = os.environ['POSTGRES_PASSWORD']
+# database = os.environ['POSTGRES_DB']
+# host = os.environ['POSTGRES_HOST']
+# port = os.environ['POSTGRES_PORT']
+user = 'admin'
+password = 'api-db'
+database = 'feira-livre'
+host = 'localhost'
+port = 5432
+
+# conn = psycopg2.connect(
+#     host=host,
+#     database=database,
+#     user=user,
+#     password=password
+# )
+
+DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+engine = db.create_engine(DATABASE_CONNECTION_URI)
+
+Base = declarative_base()
